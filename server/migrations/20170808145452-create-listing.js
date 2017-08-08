61c13adad60729eb6018e975be6b5048ddab19de
Toby Hamand <tobyhamand@gmail.com>
2017-08-08 16:19:21 +0100

    setup sequelize database

---
+++ server/migrations/20170808145452-create-listing.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Listings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING        
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Listings');
+  }
+};
