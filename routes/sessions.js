e8264552c9bb03bfb3c249319acbcd5c7705e511
Alison Woodman <aliwoodman@gmail.com>
2017-08-11 14:43:28 +0100

    Trying to logout

--- routes/sessions.js
+++ routes/sessions.js
@@ -13,6 +13,7 @@ var sessionChecker = (req, res, next) => {
 
 router.route('/')
   .get(sessionChecker, (req, res) => {
+    console.log('Hi')
     res.redirect('/sessions/new');
   })
 
@@ -22,23 +23,24 @@ router.route('/')
 
          models.User.findOne({ where: { email: email } }).then(function (user) {
               if (!user) {
+                console.log('user:' + user)
+                console.log('1')
                   res.redirect('/sessions/new');
               } else if (!user.validPassword(password)) {
+                console.log('2')
                   res.redirect('/sessions/new');
               } else {
+                console.log('3')
                   req.session.user = user.dataValues;
                   res.redirect('/');
               }
           });
       })
 
-  // .get('/logout', function(req, res) {
-  //   req.session.reset();
-  //   res.redirect('/');
-  // });
-
   .delete((req, res) => {
     if (req.session.user && req.cookies.user_sid) {
+      console.log(req.session.user)
+      console.log("I got here")
       res.clearCookie('user_sid');
     }
     res.redirect('/')
@@ -48,11 +50,11 @@ router.get('/new', function(req, res, next) {
   res.render('sessions-new', { title: "Hello" });
 });
 
-router.get('/logout', function(req, res) {
-  req.session.reset;
-  console.log(req.session.user)
-  res.redirect('/');
-});
+// router.get('/logout', function(req, res) {
+//   req.session.reset;
+//   console.log(req.session.user)
+//   res.redirect('/');
+// });
 
 
 module.exports = router;
